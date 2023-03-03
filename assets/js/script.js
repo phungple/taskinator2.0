8f85acf645491effda3a14998402a963657403ce
phungle <phungple2015@gmail.com>
2023-03-03 16:26:49 -0600

    submit event for formEl.addEventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+//by adding the event argument to the createTaskHandler(), we can use the data and functionality that object holds.
+var createTaskHandler = function(event) {
+
+    // if we execute the method below, we're instructing the browser to not carry out its default behavior
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task";
     tasksToDoEl.appendChild(listItemEl);
+    
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+//form-specific event "submit" will listens for 2 events within the form:
+// when click a <button> with a type attribute that has a value of "submit"
+// when press Enter on keyboard
+formEl.addEventListener("submit", createTaskHandler);
