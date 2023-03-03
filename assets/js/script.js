57ae7423306352b3f2873afbcd502802cbd4915f
phungle <phungple2015@gmail.com>
2023-03-03 17:30:57 -0600

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,24 @@ var createTaskHandler = function(event) {
     // if we execute the method below, we're instructing the browser to not carry out its default behavior
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create "li" item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
-    tasksToDoEl.appendChild(listItemEl);
+
+    //create div to hold task info and add to "li" item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
     
+    // add HTML content to div
+    taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 //form-specific event "submit" will listens for 2 events within the form:
