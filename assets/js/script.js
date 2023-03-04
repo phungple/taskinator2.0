872e6472aeb163fe18336afa4a1755605b7ebf36
phungle <phungple2015@gmail.com>
2023-03-03 18:16:20 -0600

    refactor the code to organize functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-//by adding the event argument to the createTaskHandler(), we can use the data and functionality that object holds.
-var createTaskHandler = function(event) {
+//by adding the event argument to the taskFormHandler(), we can use the data and functionality that object holds.
+var taskFormHandler = function(event) {
 
     // if we execute the method below, we're instructing the browser to not carry out its default behavior
     event.preventDefault();
@@ -10,6 +10,17 @@ var createTaskHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
     //create "li" item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -18,9 +29,9 @@ var createTaskHandler = function(event) {
     var taskInfoEl = document.createElement("div");
     // give it a class name
     taskInfoEl.className = "task-info";
-    
+
     // add HTML content to div
-    taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
     //add entire list item to list
@@ -30,4 +41,4 @@ var createTaskHandler = function(event) {
 //form-specific event "submit" will listens for 2 events within the form:
 // when click a <button> with a type attribute that has a value of "submit"
 // when press Enter on keyboard
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
